fc495b717d9a63c1ff0939838064069043491328
Serega <serv3546@gmail.com>
2017-12-16 13:27:37 -0500

    LoginActivity is done, added HomePageActivity

--- app/src/main/java/com/example/serega/twitterkitforandroid/LoginActivity.java
+++ app/src/main/java/com/example/serega/twitterkitforandroid/LoginActivity.java
@@ -1,13 +1,47 @@
 package com.example.serega.twitterkitforandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TwitterLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Twitter.initialize(this);
         setContentView(R.layout.login_main);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setCallback(twitterSessionCallback);
+    }
+
+    private final Callback<TwitterSession> twitterSessionCallback = new Callback<TwitterSession>() {
+        @Override
+        public void success(Result<TwitterSession> result) {
+            Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
+            startActivity(intent);
+        }
+
+        @Override
+        public void failure(TwitterException exception) {
+            Toast.makeText(LoginActivity.this, getString(R.string.error), Toast.LENGTH_SHORT).show();
+            exception.printStackTrace();
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 }

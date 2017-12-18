a619daf1c9b6517cb8739ecb60e23dc464dca982
Serega <serv3546@gmail.com>
2017-12-18 02:52:12 -0500

    added recycle view with user tweets, action to create and post new tweets

--- app/src/main/java/com/example/serega/twitterkitforandroid/activities/HomePageActivity.java
+++ app/src/main/java/com/example/serega/twitterkitforandroid/activities/HomePageActivity.java
@@ -1,11 +1,14 @@
 package com.example.serega.twitterkitforandroid.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
@@ -19,12 +22,18 @@ import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.models.User;
+import com.twitter.sdk.android.tweetcomposer.ComposerActivity;
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 import retrofit2.Call;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    private static final int MAX_ITEMS_PER_REQUEST = 200;
+    private TwitterSession session;
     private LinearLayout profileBannerLayout;
     private ImageView profileImageView;
     private TextView userNameView;
@@ -76,6 +85,27 @@ public class HomePageActivity extends AppCompatActivity {
                 .verifyCredentials(true, false, false);
 
         userData.enqueue(userCallback);
+
+        session = TwitterCore.getInstance()
+                .getSessionManager().getActiveSession();
+        long userId = session.getUserId();
+
+        RecyclerView recyclerView = findViewById(R.id.twits_recycle_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        UserTimeline userTimeline = new UserTimeline.Builder()
+                .userId(userId)
+                .maxItemsPerRequest(MAX_ITEMS_PER_REQUEST)
+                .includeReplies(true)
+                .build();
+
+        TweetTimelineRecyclerViewAdapter adapter =
+                new TweetTimelineRecyclerViewAdapter.Builder(this)
+                        .setTimeline(userTimeline)
+                        .setViewStyle(R.style.tw__TweetLightWithActionsStyle)
+                        .build();
+
+        recyclerView.setAdapter(adapter);
     }
 
     private final Callback<User> userCallback = new Callback<User>() {
@@ -129,9 +159,12 @@ public class HomePageActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.action_new_twit:
+                    final Intent intent = new ComposerActivity.Builder(HomePageActivity.this)
+                            .session(session)
+                            .createIntent();
+                    startActivity(intent);
                     break;
             }
-
         }
     };
 
